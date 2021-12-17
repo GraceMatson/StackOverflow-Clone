4f9d40a12193a1144c24bbbc5da6349b99f24f50
lolzone13 <mohitscience123@gmail.com>
2021-12-17 13:38:25 +0530

    Added routing features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css'
+import './App.css';
+import { Link } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 
 
@@ -7,6 +8,10 @@ function App(){
   return (
     <div className="App">      
       <LoginPage />
+      <nav>
+        <Link to="/Questions">Questions</Link>{" "}
+        <Link to="/Users">Users</Link>
+      </nav>
     </div>
   )
 }
