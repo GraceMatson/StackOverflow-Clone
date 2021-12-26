4b8c356dbadfe1479729000c273a45bb9d18d1f6
Srishti-Goel <43054441+Srishti-Goel@users.noreply.github.com>
2021-12-26 15:39:56 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,17 @@ import TopNav from './components/TopNav/TopNav';
 import UserProfile from './components/UserProfile/Userprofile';
 
 function App(){
-  let memberDets = {avatar_src : "https://cdn.pixabay.com/photo/2017/01/08/13/58/cube-1963036__340.jpg", userName : "ABC", userAge : 10, lastSeen : 10, streak : 0}; // Need to get this from the DB
+  let memberDets = {avatar_src : "https://cdn.pixabay.com/photo/2017/01/08/13/58/cube-1963036__340.jpg", userName : "ABC", userAge : '10 days', lastSeen : '22 Dec', activeDays : 7}; // Need to get this from the DB
   let dp_src = memberDets.avatar_src; 
-  let user_name = memberDets.userName; 
   return (
     <div className="App">
       <TopNav 
         avatar_src = {dp_src}
       />
       <UserProfile
-        memberDets = {memberDets}
-      />
-      <LoginPage
-      />
-      Should there be something here?
+          memberDets = {memberDets}
+        />
+      <LoginPage/>
     </div>
   )
 }
