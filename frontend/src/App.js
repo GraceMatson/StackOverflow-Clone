749bdea53b13d01966645b1214e29c2836e1141b
Srishti-Goel <43054441+Srishti-Goel@users.noreply.github.com>
2021-12-26 15:23:40 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,18 @@ import TopNav from './components/TopNav/TopNav';
 import UserProfile from './components/UserProfile/Userprofile';
 
 function App(){
+  let dp_scr = "https://cdn.pixabay.com/photo/2017/01/08/13/58/cube-1963036__340.jpg"; // Need to get this from the DB
+  let user_name = "ABC"; // Need to get this from the DB
+  let dp_scr = "https://cdn.pixabay.com/photo/2017/01/08/13/58/cube-1963036__340.jpg"; // Need to get this from the DB
   return (
     <div className="App">
       <TopNav 
-        avatar_src = "https://cdn.pixabay.com/photo/2017/01/08/13/58/cube-1963036__340.jpg"
+        avatar_src = {dp_src}
       />
 
-      <UserProfile/>
-
-      <LoginPage />
+      <LoginPage
+        avatar_src = {dp_scr}
+      />
       Should there be something here?
     </div>
   )
